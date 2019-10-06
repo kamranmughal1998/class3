500dde0aa821840a8c12f7af5b7e5252d8fcf426
kamranmughal1998 <mughalkamran1998@gmail.com>
2019-10-06 17:34:37 +0500

    class 3

---
+++ class3a/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{width:"100%",flex:0.5,backgroundColor:"red",alignItems:"center"}}>
+      <Text style={{fontSize:10}}>Open</Text>
+    </View>
+  );
+}
